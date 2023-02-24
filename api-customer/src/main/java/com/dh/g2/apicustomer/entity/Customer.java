5e4ffd7797eefa68c56d207ea7a40ec1a876f792
Damián Micheli <damianmicheli@gmail.com>
2023-02-24 13:43:16 -0300

    Arreglar customer entity

--- api-customer/src/main/java/com/dh/g2/apicustomer/entity/Customer.java
+++ api-customer/src/main/java/com/dh/g2/apicustomer/entity/Customer.java
@@ -2,35 +2,31 @@ package com.dh.g2.apicustomer.entity;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 import lombok.Setter;
-
 import javax.persistence.*;
+import java.time.LocalDate;
+
 
 
 @Getter @Setter @NoArgsConstructor
 @Entity
-
 @Table(name = "customers")
 public class Customer {
-
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @NotBlank
-    @Column(name = "id_type")
+    @NonNull
     private String idType;
     @NonNull
-    @Column(name = "id_num")
     private int idNum;
     @NonNull
-    @Column(name = "first_name")
     private String firstName;
     @NonNull
-    @Column(name = "last_name")
     private String lastName;
+    @NonNull
     private String gender;
-    @Column(name = "date_of_birth")
-    private LocalDate birthDate;
+    @NonNull
+    private LocalDate birthdate;
 }
